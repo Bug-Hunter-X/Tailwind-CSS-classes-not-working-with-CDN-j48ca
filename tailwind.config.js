01b357a84b204b045c8ef9cd051b595cd7bbcad0
BugHunter-X <askzenai@gmail.com>
2025-01-18 00:06:07 +0100

    Add tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+```html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Tailwind CSS Bug Solution</title>
+</head>
+<body>
+  <div class="bg-red-500 p-4">
+    <p>This is some text.</p>
+  </div>
+  <script src="https://cdn.tailwindcss.com"></script>
+</body>
+</html>
+```
